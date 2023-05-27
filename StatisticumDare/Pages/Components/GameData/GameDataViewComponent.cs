a384d7a3b00decdc91d196fcbc14520121bb3839
Brenden Bunker <brendenbunker@gmail.com>
2023-05-27 11:06:49 -0600

    Moved the table row to a component

---
+++ StatisticumDare/Pages/Components/GameData/GameDataViewComponent.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Projects.LudumDare.ViewModels;
+
+namespace StatisticumDare.Pages.Components.GameData
+{
+    [ViewComponent]
+    public class GameDataViewComponent : ViewComponent
+    {
+        public GameDataViewComponent()
+        {
+            // we can do what we like here, including using Dependency Injection to bring in services and what not
+        }
+        
+        public IViewComponentResult Invoke(CategoryModel category)
+        {
+            // here we have access to any parameters passed in when we render the ViewComponent (name in this case)
+            // so we could do something like this for example...
+            
+            // _someService.GetUserDetails(name)
+            // although in reality you'd probably want to accept an id rather than a name!
+            
+            return View("Default", category);
+        }
+    }
+
+    public class CategoryModel {
+        public Category data { get; set; }
+        public string name { get; set; }
+
+        public CategoryModel(string name, Category data) {
+            this.name = name;
+            this.data = data;
+        }
+    }
+}
